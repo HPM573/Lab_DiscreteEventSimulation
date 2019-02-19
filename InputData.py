5bca24b1484e3d07f606eb1758e6287ef8bb5328
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-02-18 20:16:48 -0500

    p1

--- InputData.py
+++ InputData.py
@@ -1,8 +1,4 @@
 
-# trace
-TRACE_ON = True        # Set to true to trace a simulation replication
-DECI = 3                # the decimal point to round the numbers to in the trace file
-
 # simulation settings
 SIM_DURATION = 100000   # (hours) a large number to me sure the simulation will be terminated eventually but
                         # the simulation continues as long as there is a patient in the urgent care.
