9d0c859ff44d6b3d2cd898e2ba53cf8b240b78ad
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-24 10:41:41 -0500

    warm-up

--- InputData.py
+++ InputData.py
@@ -6,8 +6,8 @@ DECI = 5                # the decimal point to round the numbers to in the trace
 # simulation settings
 SIM_DURATION = 100000   # (hours) a large number to make sure the simulation will be terminated eventually but
                         # the simulation continues as long as there is a patient in the urgent care.
-HOURS_OPEN = 7*24                 # hours the urgent cares open
-WARM_UP = 2*24                  # warm up period
+HOURS_OPEN = 30*24                 # hours the urgent cares open
+WARM_UP = 5*24                  # warm up period
 N_EXAM_ROOMS = 20                # number of exam rooms
-MEAN_ARRIVAL_TIME = 10/60/20        # mean patients inter-arrival time (hours)
-MEAN_EXAM_DURATION = 10/60       # mean of exam duration (hours)
+MEAN_ARRIVAL_TIME = 20/60/20        # mean patients inter-arrival time (hours)
+MEAN_EXAM_DURATION = 20/60       # mean of exam duration (hours)
