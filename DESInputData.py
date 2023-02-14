8ab82e3006b0eaf03c364333b813592912006df7
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-02-13 21:17:17 -0500

    Update DESInputData.py

--- DESInputData.py
+++ DESInputData.py
@@ -3,6 +3,6 @@
 SIM_DURATION = 100000   # (hours) a large number to make sure the simulation will be terminated eventually but
                         # the simulation continues as long as there is a patient in the urgent care.
 HOURS_OPEN = 20                 # hours the urgent cares open
-N_PHYSICIANS = 2                # number of exam rooms
+N_PHYSICIANS = 2                # number of physicians
 MEAN_ARRIVAL_TIME = 5/60        # mean patients inter-arrival time (hours)
 MEAN_EXAM_DURATION = 8/60       # mean of exam duration (hours)
